c615c1846f8086e7d7fe9efcf6d981c3192f09fe
tlubowiecki <tomasz@lubowiecki.de>
2025-11-26 11:56:55 +0100

    Filter und CSS

--- src/main/java/de/gfn/dbdemo/TodoController.java
+++ src/main/java/de/gfn/dbdemo/TodoController.java
@@ -1,9 +1,12 @@
 package de.gfn.dbdemo;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -13,6 +16,7 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 
 public class TodoController implements Initializable {
 
@@ -21,6 +25,8 @@ public class TodoController implements Initializable {
 
     @FXML
     private TextField eingabe;
+
+    private Predicate<Todo> filter;
 
     @FXML
     protected void eingabeVerarbeiten(KeyEvent event) throws SQLException {
@@ -41,14 +47,51 @@ public class TodoController implements Initializable {
             TodoRepository.update(todo);
             updateAusgabe();
         }
+        else if(event.getCode() == KeyCode.BACK_SPACE) {
+            Todo todo = todos.getSelectionModel().getSelectedItem();
+            TodoRepository.delete(todo);
+            updateAusgabe();
+        }
+    }
+
+    @FXML
+    protected void filter(ActionEvent event) throws SQLException {
+
+        // Welcher Button wurde geklick?
+        String auswahl = ((Button)event.getSource()).getText();
+
+        // Filter einsetzen
+        switch(auswahl) {
+            case "offen":
+                filter = t -> !t.isDone();
+                break;
+
+            case "erledigt":
+                filter = t -> t.isDone();
+                break;
+
+            default:
+                filter = null;
+        };
+
+        // Ausgabe mit Filter vornehmen
+        updateAusgabe();
     }
 
     private void updateAusgabe() throws SQLException {
         // todos = lokale Variable
         // this.todos = Instanzvariable
-        List<Todo> todos = TodoRepository.findAll();
         // FXCollections.observableList verpackt eine "normale" Liste in eine ObservableList
-        this.todos.setItems(FXCollections.observableList(todos));
+        ObservableList<Todo> todos = FXCollections.observableList(TodoRepository.findAll());
+
+        if(filter == null) {
+            // Ohne Filter
+            this.todos.setItems(todos);
+        }
+        else {
+            // Mit Filter
+            this.todos.setItems(new FilteredList<>(todos, filter));
+        }
     }
 
 
